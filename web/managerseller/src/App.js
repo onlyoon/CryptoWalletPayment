8af010f8528c0f11e8306095cc7c648541595c0d
parkyoon <parkyoon97@gmail.com>
2023-05-09 19:46:02 +0900

    managerseller

--- web/managerseller/src/App.js
+++ web/managerseller/src/App.js
@@ -1,16 +1,15 @@
-import Container1 from "./routePages/Container1";
-import Container2 from "./routePages/Container2";
-import Container3 from "./routePages/Container3";
-import Container4 from "./routePages/Container4";
-import Container5 from "./routePages/Container5";
-import Container6 from "./routePages/Container6";
-import Container7 from "./routePages/Container7";
-import Container8 from "./routePages/Container8";
+import Container1 from './routePages/Container1';
+import Container2 from './routePages/Container2';
+import Container3 from './routePages/Container3';
+import Container4 from './routePages/Container4';
+import Container5 from './routePages/Container5';
+import Container6 from './routePages/Container6';
+import Container7 from './routePages/Container7';
+import Container8 from './routePages/Container8';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
-
   return (
     <BrowserRouter>
       <Routes>
